ede72da4cf44f4448f87af622686cedec30832b9
ADETOLAR01 <deniyibanji2@gmail.com>
2022-09-08 08:34:55 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -19,12 +19,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle"); return (0);
 
 }
-
-
-
-:wq
-chmod u+x 4-puts.c
-git add .
-git commit -m 'Hello puts'
-git push
-#git push
