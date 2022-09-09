6763d92eab929b498b9918a79d0f7681763ae995
ADETOLAR01 <deniyibanji2@gmail.com>
2022-09-09 06:14:19 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
 /**
-
-* main - print the string in the put function
-
-*
-
-* Description: using the main function
-
-* this program prints "Programming is like building a multilingual puzzle
-
-* Return: 0
-
-*/
-
+ * Description: main - Prints out a string to stdout.
+ *
+ * Return: 0 if success. 
+ *
+ */
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-	puts("\"Programming is like building a multilingual puzzle"); return (0);
-
+	return (0);
 }
