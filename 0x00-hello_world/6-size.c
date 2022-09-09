2103a0aba1d5d00696d84ea0a95fccd347eae319
ADETOLAR01 <deniyibanji2@gmail.com>
2022-09-09 07:10:47 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,42 +1,22 @@
+#include <stdio.h>
+
 /**
-
-* main - print the string in the put function
-
-*
-
-* Description: using the main function
-
-* this program prints "Programming is like building a multilingual puzzle
-
-* Return: 0
-
-*/
-
-int main(void)
-
+ * Main - Prints the sizeof many var types
+ * 
+ * Return: Always 0 (success)
+ */
+int Main(void)
 {
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	char c;
-
-	int i;
-
-	long li;
-
-	long long lli;
-
-	float f;
-
-
-
-	printf("Size of a char: %ld bytes(s)\n", sizeof(c));
-
-	printf("Size of a int i: %ld bytes(s)\n", sizeof(i));
-
-	printf("Size of a long li: %ld bytes(s)\n", sizeof(li));
-
-	printf("Size of a long long lli: %ld bytes(s)\n", sizeof(lli));
-
-	printf("Size of a float f: %ld bytes(s)\n", sizeof(f)); return (0)
-
-}
-
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+	return (0);
+}    	
