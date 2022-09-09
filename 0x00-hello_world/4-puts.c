196c72dae92c1322a0bb6bcf9e8bc2d02ad4d574
ADETOLAR01 <deniyibanji2@gmail.com>
2022-09-09 06:23:37 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 /**
-* main - Prints out a string to stdout.
+* main - Printng
 *
-* Return: 0 if success. 
-*
+* Return: Always 0 (successful) 
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
