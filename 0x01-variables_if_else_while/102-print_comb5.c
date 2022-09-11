b69cfa46ceb383b5af95236363cc60de4fa7cadc
ADETOLAR01 <111161031+ADETOLAR01@users.noreply.github.com>
2022-09-11 21:03:33 +0100

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {
-	int i, p;
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
-		for (p = 0; p < 100; j++)
+		for (j = 0; j < 100; j++)
 		{
-			if (p > i)
+			if (j > i)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
 				putchar(' ');
-				putchar((p / 10) + '0');
-				putchar((p % 10) + '0');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
 
 				if (i != 98)
 				{
