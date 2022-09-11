79d56525b3cd21c139efa43e44ecec2dd9e6c1dd
ADETOLAR01 <111161031+ADETOLAR01@users.noreply.github.com>
2022-09-11 21:09:24 +0100

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,9 @@ int main(void)
                 {                                                                                                                      
                         if (j > i)                                                                                                     
                         {                                                                                                              
-                                putchar(i + '0');                                                                                      
-                                putchar(j + '0');                                                                                      
+                                putchar((i + '0');                                                                                      
+                                putchar((j + '0');
+                                        
                                 if (i != 8)                                                                                            
                                 {                                                                                                      
                                         putchar(',');                                                                                  
