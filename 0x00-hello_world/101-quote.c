ccba175e7c8ee85933ca0ebb3c5f2bc08bcb8f6a
ADETOLAR01 <111161031+ADETOLAR01@users.noreply.github.com>
2022-09-11 16:51:38 +0100

    UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
-/**
-* Description: main - Prints out a string to stdout.
-*
-* Return: 0 if success. 
-*/
-int main(void)
-{
-write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n",59);
-return (1);
-}
+#include <stdio.h> 
+  
+ /** 
+  * main - Entry point 
+  * 
+  * Return: Always 0 (Success) 
+  */ 
+ int main(void) 
+ { 
+         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61); 
+           return (1); 
+ }
